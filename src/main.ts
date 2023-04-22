ed2b1e1448efbf225369497dc030da272a14a15a
louiiuol <louis.godlewski@vidmizer.com>
2023-04-22 18:11:13 +0200

    🔧 Completed Bootstrap fn w/ essentials deps

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { environment } from './app/environment';
+import { GlobalExceptionFilter } from './app/core/exceptions/global-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new GlobalExceptionFilter());
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = environment.API_PORT || 3333;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://${environment.API_HOST}:${port}/${globalPrefix}`,
+  );
 }
 bootstrap();
