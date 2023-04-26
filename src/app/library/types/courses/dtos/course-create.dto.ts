74dfd6dadb69cf14ceff5afb86b651bfc4bac30a
Louis Godlewski <louis.godlewski@gmail.com>
2023-04-27 01:14:34 +0200

    🐛  Fixed path for files

--- src/app/library/types/courses/dtos/course-create.dto.ts
+++ src/app/library/types/courses/dtos/course-create.dto.ts
@@ -22,11 +22,11 @@ export class CourseCreateDto {
 			.reduce((p, t) => p.concat(t.toLocaleUpperCase()), [])
 			.join('-')}/`;
 		this.name = lesson.phonemes.join(' ');
-		this.lesson = path + '/leçon.pdf';
-		this.script = path + '/script.pdf';
-		this.exercice = path + '/exercice.pdf';
-		if (lesson.poster) this.poster = path + '/poster.pdf';
-		if (lesson.text) this.text = path + '/text.pdf';
+		this.lesson = path + 'leçon.pdf';
+		this.script = path + 'script.pdf';
+		this.exercice = path + 'exercice.pdf';
+		if (lesson.poster) this.poster = path + 'poster.pdf';
+		if (lesson.text) this.text = path + 'text.pdf';
 		this.phonemes = lesson.phonemes.map(
 			phoneme => new PhonemeCreateDto(phoneme, path)
 		);
