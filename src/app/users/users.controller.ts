eee3212f20ee8b5b1f0c3e58cb5ab9d79bbbefb4
Louis Godlewski <louis.godlewski@gmail.com>
2023-05-01 18:52:30 +0200

    🎨 Improved user module

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -1,25 +1,25 @@
-import { Request, Controller, Get, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { User, UserViewDto } from './types';
-import { InjectMapper } from '@automapper/nestjs';
-import { Mapper } from '@automapper/core';
-import { UsersService } from './users.service';
+import {Request, Controller, Get, UseGuards} from '@nestjs/common';
+import {JwtAuthGuard} from '../auth/guards/jwt-auth.guard';
+import {User, UserViewDto} from './types';
+import {InjectMapper} from '@automapper/nestjs';
+import {Mapper} from '@automapper/core';
+import {UsersService} from './users.service';
 
 @Controller()
 export class UsersController {
-  constructor(
-    @InjectMapper() private readonly classMapper: Mapper,
-    private readonly usersService: UsersService,
-  ) {}
+	constructor(
+		@InjectMapper() private readonly classMapper: Mapper,
+		private readonly usersService: UsersService
+	) {}
 
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  async getProfile(@Request() req): Promise<UserViewDto> {
-    const user = await this.usersService.findOneByEmail(req.user.email);
-    return this.classMapper.map(user, User, UserViewDto);
-  }
+	@UseGuards(JwtAuthGuard)
+	@Get('profile')
+	async getProfile(@Request() req): Promise<UserViewDto> {
+		const user = await this.usersService.findOneByEmail(req.user.email);
+		return this.classMapper.map(user, User, UserViewDto);
+	}
 
-  // TODO Update user
+	// TODO Update user
 
-  // TODO Close account (archive user)
+	// TODO Close account (archive user)
 }
