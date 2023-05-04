258850aa4e7c7d8d8cb46fb5df51aa9703fdadb6
Louis Godlewski <louis.godlewski@gmail.com>
2023-05-04 21:44:25 +0200

    ♻️ Refactoring controller

--- src/app/library/library.controller.ts
+++ src/app/library/library.controller.ts
@@ -1,15 +1,19 @@
-import {Controller, Get, Post} from '@nestjs/common';
+import {Controller, Get, Post, UseGuards} from '@nestjs/common';
 import {LibraryService} from './library.service';
+import {JwtAuthGuard} from '../auth/guards/jwt/jwt-auth.guard';
+import {AdminGuard} from '../auth/guards/roles/admin.guard';
 
 @Controller()
 export class LibraryController {
 	constructor(private libraryService: LibraryService) {}
 
+	@UseGuards(JwtAuthGuard, AdminGuard)
 	@Post('courses')
 	async generateLibrary() {
 		return await this.libraryService.createLibrary();
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Get('courses')
 	async getAllCourses() {
 		return await this.libraryService.getAllCourses();
